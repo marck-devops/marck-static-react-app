0b2d2ba91aecc06bfbe775e6a7a3f00650361ea5
marck-devops <marck.devops@gmail.com>
2025-07-02 11:14:20 -0400

    Push react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Marck's Static React App!</h1>
+      <p>This app is deployed on S3 + CloudFront.</p>
+    </div>
+  );
+}
+
+export default App;
