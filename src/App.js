3091318d0832989fd643d6203fc691d31f2b1f80
marck-devops <marck.devops@gmail.com>
2025-07-06 14:54:06 -0400

    added message to test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Marck's Static React App!</h1>
+      <h1>Welcome to Marck's Static React App on CloudFront. GitHub Actions course completed!</h1>
       <p>This app is deployed on S3 + CloudFront.</p>
     </div>
   );
